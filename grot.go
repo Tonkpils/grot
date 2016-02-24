7a45a66bcc26abb71d0b0df1d2d140304b201c50
Leo Correa <lcorr005@gmail.com>
2016-02-24 11:51:09 -0500

    Reorganize to setup into package

---
+++ grot.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+var (
+	chatAdapterMu sync.RWMutex
+	chatAdapter   ChatAdapter
+)
+
+func RegisterChatAdapter(adapter ChatAdapter) {
+	chatAdapterMu.Lock()
+	defer chatAdapterMu.Unlock()
+	if chatAdapter != nil {
+		log.Panic("chatbot: cannot load multiple chat adapters")
+	}
+
+	chatAdapter = adapter
+}
+
+type ChatAdapter interface {
+	// Send is used to send a message to the chat source from the robot.
+	// This will be used by listeners to respond to messages
+	Send(message string) error
+	// Receive ensures the adapter begins listening on the chat source.
+	Receive(bot *Bot) error
+}
+
+type Bot struct {
+	Client ChatAdapter
+	Logger *log.Logger
+	Router *http.ServeMux
+}
+
+func NewBot() *Bot {
+	logger := log.New(os.Stdout, "", 0)
+	router := http.NewServeMux()
+
+	return &Bot{
+		Client: chatAdapter,
+		Logger: logger,
+		Router: router,
+	}
+}
+
+func (b *Bot) Run() error {
+	go b.Client.Receive(b)
+
+	b.Router.HandleFunc("/grot/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("PONG"))
+	})
+
+	// TODO: create routing structure to allow providing handlers
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), b.Router); err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -2,68 +2,16 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
-	"sync"
 )
-
-var (
-	chatAdapterMu sync.RWMutex
-	chatAdapter   ChatAdapter
-)
-
-func RegisterChatAdapter(adapter ChatAdapter) {
-	chatAdapterMu.Lock()
-	defer chatAdapterMu.Unlock()
-	if chatAdapter != nil {
-		log.Panic("grot: cannot load multiple chat adapters")
-	}
-
-	chatAdapter = adapter
-}
 
 func init() {
 	log.SetOutput(os.Stdout)
 	log.SetFlags(0)
 }
 
-type ChatAdapter interface {
-	// Send is used to send a message to the chat source from the robot.
-	// This will be used by listeners to respond to messages
-	Send(message string) error
-	// Receive ensures the adapter begins listening on the chat source.
-	Receive(bot *Bot) error
-}
-
-type Bot struct {
-	Client ChatAdapter
-	Logger *log.Logger
-}
-
-func NewBot() *Bot {
-	logger := log.New(os.Stdout, "", 0)
-	return &Bot{
-		Client: chatAdapter,
-		Logger: logger,
-	}
-}
-
-func (b *Bot) Run() error {
-	go b.Client.Receive(b)
-
-	// TODO: create routing structure to allow providing handlers
-	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func main() {
 	log.Println("system going online...")
-	// Configuration ....
-	// - Chat adapter
-	// - Additional command adapters
 	bot := NewBot()
 
 	if err := bot.Run(); err != nil {
